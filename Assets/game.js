0ae3af2f0f22bbf5ee2087649185302c98f5bc26
Holger Mueller <9jholger@gmail.com>
2018-03-08 11:24:56 -0600

    rndm generators created

--- Assets/game.js
+++ Assets/game.js
@@ -1,8 +1,23 @@
 'use strict';
 
-//program generates a random number
+//program generates a random number between 19 and 120
 
-//each crystal has a hidden value i.e. random number
+const randNum = Math.floor(Math.random()*120)+19;
+console.log(randNum);
+
+//each crystal has a hidden value i.e. random number between1 and 12
+
+const pearl = Math.floor(Math.random()*12)+1;
+//console.log(pearl);
+
+const emld = Math.floor(Math.random()*12)+1;
+//console.log(emld);
+
+const ruby = Math.floor(Math.random()*12)+1;
+//console.log(ruby);
+
+const sapphire = Math.floor(Math.random()*12)+1;
+//console.log(sapphire);
 
 //player clicks the crystals
 
