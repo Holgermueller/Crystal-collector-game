ba294060b173cac7b149fede7faafd65b6dbfafd
Holger Mueller <9jholger@gmail.com>
2018-03-07 22:32:03 -0600

    game scaffolding

--- Assets/game.js
+++ Assets/game.js
@@ -1,1 +1,16 @@
 'use strict';
+
+//program generates a random number
+
+//each crystal has a hidden value i.e. random number
+
+//player clicks the crystals
+
+//values are totalled in totScore
+
+//script determines if player wins or loses
+
+
+//outcome recorded in winsLosses
+
+//game restars, page does NOT reload
