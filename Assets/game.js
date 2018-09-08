49956925e7b776d75d54b13dcbe3338ed2da8b33
Holger Mueller <9jholger@gmail.com>
2018-09-07 22:32:49 -0500

    cleaned up and refactored code

--- Assets/game.js
+++ Assets/game.js
@@ -1,135 +1,108 @@
 'use strict';
 
-$(document).ready(function(){
+$(document).ready(() => {
 
-//program generates a random number between 19 and 120
-
-let randNumber = Math.floor(Math.random() * ((120-19)+1) + 19);
-$('#randNum').html(randNumber);
-    
-console.log(randNumber); //it works!!!
-
-//each crystal has a hidden value i.e. random number between1 and 12
-
-let amethyst = Math.floor(Math.random()*12)+1;
-
-console.log(amethyst);
-
-let emld = Math.floor(Math.random()*12)+1;
-
-console.log(emld);
-
-let ruby = Math.floor(Math.random()*12)+1;
-
-console.log(ruby);
-
-let sapphire = Math.floor(Math.random()*12)+1;
-
-console.log(sapphire);
-
-//player clicks the crystals to display value to totScore
-
-let wins = 0; //#wins
-let losses = 0;  //#losses
-
-let totScore = 0;
-$('#totScore').text(totScore);
-console.log(totScore);
-
-function win() {
-    wins = wins+1;
-    $('#wins').text(wins);
-    resetTwo();
-    reset();
-}
-
-function lose() {
-    losses = losses+1;
-    $('#losses').text(losses);
-    resetTwo();
-    reset();
-}
-
-$('#amethyst').click(function() {
-    totScore = totScore + amethyst
-    $('#totScore').text(totScore);
-    //console.log(pearl);
-    if ( totScore === randNumber ) {
-        win();
-        reset();
-        resetTwo();
-       } else if ( totScore > randNumber ) {
-        lose();
-        reset();
-        resetTwo();
-   }  
-})
-
-$('#emld').click(function() {
-    totScore = totScore + emld;
-    $('#totScore').text(totScore);
-    //console.log(emld);
-    if ( totScore === randNumber ) {
-        win();
-        reset();
-        resetTwo();
-    } else if ( totScore > randNumber ) {
-        lose();
-        reset();
-        resetTwo();
-   }
-})
-
-$('#ruby').click(function() {
-    totScore = totScore + ruby;
-    $('#totScore').text(totScore);
-    //console.log(ruby);
-    if ( totScore === randNumber ) {
-        win();
-        reset();
-        resetTwo();
-       } else if ( totScore > randNumber ) {
-        lose();
-        reset();
-        resetTwo();
-    }
-})
-
-$('#sapphire').click(function() {
-    totScore = totScore + sapphire;
-    $('#totScore').text(totScore);
-    //console.log(sapphire);
-    if ( totScore === randNumber ) {
-        win();
-        reset();
-        resetTwo();
-       } else if ( totScore > randNumber ) {
-        lose();
-        reset();
-        resetTwo();
-   }
-})
-
-//random numbers (jewels) get added together: look above
-//script determines if player wins or loses and outcome recorded in winsLosses
-
-console.log(wins);
-console.log(losses);
-
-//game restarst, page does NOT reload
-
-function reset () {
-    totScore = 0;
-    $('#totScore').text(totScore);
-}
-
-function resetTwo () {
-    randNumber = Math.floor(Math.random() * ((120-19)+1) + 19);
+    //program generates a random number between 19 and 120
+    let randNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
     $('#randNum').html(randNumber);
-    amethyst = Math.floor(Math.random()*12)+1;
-    emld = Math.floor(Math.random()*12)+1;
-    ruby = Math.floor(Math.random()*12)+1;
-    sapphire = Math.floor(Math.random()*12)+1;
-}
 
+    //each crystal has a hidden value i.e. random number between1 and 12
+    let amethyst = Math.floor(Math.random() * 12) + 1;
+    let emld = Math.floor(Math.random() * 12) + 1;
+    let ruby = Math.floor(Math.random() * 12) + 1;
+    let sapphire = Math.floor(Math.random() * 12) + 1;
+
+    //player clicks the crystals to display value to totScore
+    let wins = 0; //#wins
+    let losses = 0;  //#losses
+
+    let totScore = 0;
+    $('#totScore').text(totScore);
+
+    function win() {
+        wins = wins + 1;
+        $('#wins').text(wins);
+        resetTwo();
+        reset();
+    }
+
+    function lose() {
+        losses = losses + 1;
+        $('#losses').text(losses);
+        resetTwo();
+        reset();
+    }
+
+    $('#amethyst').click(() => {
+        totScore = totScore + amethyst
+        $('#totScore').text(totScore);
+        if (totScore === randNumber) {
+            win();
+            reset();
+            resetTwo();
+        } else if (totScore > randNumber) {
+            lose();
+            reset();
+            resetTwo();
+        }
+    });
+
+    $('#emld').click(() => {
+        totScore = totScore + emld;
+        $('#totScore').text(totScore);
+        if (totScore === randNumber) {
+            win();
+            reset();
+            resetTwo();
+        } else if (totScore > randNumber) {
+            lose();
+            reset();
+            resetTwo();
+        }
+    });
+
+    $('#ruby').click(() => {
+        totScore = totScore + ruby;
+        $('#totScore').text(totScore);
+        if (totScore === randNumber) {
+            win();
+            reset();
+            resetTwo();
+        } else if (totScore > randNumber) {
+            lose();
+            reset();
+            resetTwo();
+        }
+    });
+
+    $('#sapphire').click(() => {
+        totScore = totScore + sapphire;
+        $('#totScore').text(totScore);
+        if (totScore === randNumber) {
+            win();
+            reset();
+            resetTwo();
+        } else if (totScore > randNumber) {
+            lose();
+            reset();
+            resetTwo();
+        }
+    });
+
+    //random numbers (jewels) get added together: look above
+    //script determines if player wins or loses and outcome recorded in winsLosses
+    //game restarst, page does NOT reload
+    function reset() {
+        totScore = 0;
+        $('#totScore').text(totScore);
+    }
+
+    function resetTwo() {
+        randNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+        $('#randNum').html(randNumber);
+        amethyst = Math.floor(Math.random() * 12) + 1;
+        emld = Math.floor(Math.random() * 12) + 1;
+        ruby = Math.floor(Math.random() * 12) + 1;
+        sapphire = Math.floor(Math.random() * 12) + 1;
+    }
 });
